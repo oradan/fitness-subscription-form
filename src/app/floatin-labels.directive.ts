0e68990bb5100d76b8b698d8a4eec20cc4ed457c
oradan <plotnica@yahoo.com>
2020-03-18 13:21:51 +0200

    Add floating-labels directive

--- src/app/floatin-labels.directive.ts
+++ src/app/floatin-labels.directive.ts
@@ -1,12 +1,22 @@
-import { Directive, ElementRef } from '@angular/core';
+import { Directive, ElementRef, HostListener } from '@angular/core';
 
 @Directive({
-  selector: '[floatinLabels]'
+  selector: '[floatingLabels]'
 })
 export class FloatinLabelsDirective {
 
-  constructor(private elRef:ElementRef) {
-    console.log(this.elRef.nativeElement)
-   }
-
+  constructor(private elRef: ElementRef) { }
+  @HostListener('focus', ['$event.srcElement.labels[0]'])
+  onFocus(el) {
+    el.classList.add("focused")
+  }
+  @HostListener('blur', ['$event.srcElement.labels[0]'])
+  onBlur(el) {
+    this.elRef.nativeElement.value.replace(/\s/g, "");
+    console.log(this.elRef.nativeElement.value === "")
+    console.log(this.elRef.nativeElement.value.replace(/\s/g, "").length)
+    if (this.elRef.nativeElement.value === "") {
+      el.classList.remove("focused");
+    }
+  }
 }
